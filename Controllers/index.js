7632a17043059ba295054e406383b5066a276623
Nandasagarre <62168236+Nandasagarre@users.noreply.github.com>
2023-03-07 23:39:26 +0530

    Update index.js
    Tested the payload, and added error handling method as a callback.

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,25 +1,29 @@
 const Inventory = require('../Models/Inventory');
 
-module.exports.InventoryPosting = function (req, res) {
-    //productarray should have productid, quantity, opration feild
-   // const expample = [{ productId: 1, quantity: 2, operation: 'add' }, { productId: 1, quantity: 2, operation: 'add' }];
-    const productarray = req.body.productArray;
-    const query = productarray.map(({ productId, quantity, operation }) =>  ({
-       
-        updateOne: {
-            filter: { 'Inventory.productId': productId },
-            update: {
-                $inc: {
-                    'Inventory.quantity': (operation === 'add' ? quantity : -quantity)
-                }
-            }
-        },
-        upsert:true  
-    }));
+module.exports.InventoryPosting = async function (req, res) {
+    //example
+    //const payload = [
+    //    { productId: 123, quantity: 10, operation: 'add' },
+    //    { productId: 143, quantity: 14, operation: 'add' },
+    //    { productId: 193, quantity: 17, operation: 'subtract' }
+    //];
+    // this will insert the documet if the product is new... upsert:true
 
-    Inventory.bulkWrite(query);
-    //console.log(productarray)
-    res.json({
-        message: "Done"
-        })
+
+    //Assuming that simialar to payload array is sent trough the req
+
+    const payload = req.body.productArray;
+
+
+    payload.forEach(async (item) => {
+    await Inventory.updateOne({ productId: item.productId },
+            { $inc: { quantity: (item.operation === 'add' ? item.quantity : -item.quantity) } },
+            { upsert: true },
+        (err) => {
+            if (err) {
+                return res.json({ message: "Someting went wrong, please try again later" });
+            }
+        });
+    })
+    res.json({ message: "Inventory Updated Sucessfully" });
 }
